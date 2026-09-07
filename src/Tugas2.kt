9111b85d0f3e01eb815dfec3161eceaffb97b6fe
Nuelll20 <michaelangelopji20@gmail.com>
2026-09-07 11:49:20 +0700

    Tugas 2 — Konversi Suhu

---
+++ src/Tugas2.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val celcius: Double = 30.0
+    val fahrenheit= (celcius * 9/5) + 32
+    val kelvin = celcius + 273.15
+
+    println("Celcius : $celcius c")
+    println("Fahrenheit : $fahrenheit f")
+    println("Kelvin : $kelvin k")
+}
