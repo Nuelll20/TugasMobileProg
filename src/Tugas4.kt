fc0c95b8d9e3479d407d28003fec1eddfb575427
Nuelll20 <michaelangelopji20@gmail.com>
2026-09-07 12:16:17 +0700

    Tugas 4- Luas dan Keliling lingkaran

---
+++ src/Tugas4.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val r: Double = 7.0
+    val pi: Double = 3.14159
+
+    val luas: Double = pi * r * r
+    val keliling: Double = 2 * pi * r
+
+    println("Luas lingkaran: $luas")
+    println("Keliling lingkaran: $keliling")
+}
