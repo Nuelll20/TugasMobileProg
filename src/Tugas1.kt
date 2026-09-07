d088afa06a8d02e608dd4cd3dd5fc0d2f701d30f
Nuelll20 <michaelangelopji20@gmail.com>
2026-09-07 11:33:56 +0700

    Tugas 1 — Biodata Mahasiswa

---
+++ src/Tugas1.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val nama = "Nuel"
+    var umur = 19
+    var tinggi: Double =168.5
+    var inAktif: Boolean = true
+    var golonganDarah = "B"
+
+    println("Nama   : $nama")
+    println("Umur   : $umur")
+    println("Tinggi badan  : $tinggi")
+    println("Mahasiswa aktif  : $inAktif")
+    println("Golongan Darah  : $golonganDarah")
+}
+
