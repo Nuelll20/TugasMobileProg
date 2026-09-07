f9ed4c0ac22286d4b3bbe0a58cccca524eb6975b
Nuelll20 <michaelangelopji20@gmail.com>
2026-09-07 13:39:04 +0700

    Tugas 1B- Penggunaan Val dan Var

---
+++ src/TugasB/TugasB1.kt
@@ -0,0 +1,25 @@
+package TugasB
+
+fun main(){
+    val nama: String = "Nuel"
+    val NPM: String = "245314003"
+
+    var semester: Int = 2
+    var IPK: Double = 3.40
+
+    println("Data Semester 2: ")
+    println("Nama    : $nama")
+    println("NPM     : $NPM")
+    println("Semester: $semester")
+    println("IPK     : $IPK")
+
+    semester = 3
+    IPK = 3.55
+    println("Data semester 3: ")
+    println("Nama    : $nama")
+    println("NPM     : $NPM")
+    println("Semester: $semester")
+    println("IPK     : $IPK")
+
+
+}
