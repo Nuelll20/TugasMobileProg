e52ab1cdfda40f86b03772c88e158d1b59007dd3
Nuelll20 <michaelangelopji20@gmail.com>
2026-09-12 15:33:32 +0700

    Tugas C3 - Fungsi Genap atau Ganjil

---
+++ src/TugasC/TugasC3.kt
@@ -0,0 +1,17 @@
+package TugasC
+
+fun cekGenapGanjil(bilangan: Int): String{
+    return if (bilangan % 2 == 0){
+        "Genap"
+    }else{
+        "Ganjil"
+    }
+}
+
+fun main(){
+    print("Masukkan bilangan: ")
+    val bilangan = readln(). toInt()
+
+    val hasil = cekGenapGanjil(bilangan)
+    print("$bilangan merupakan bilangan $hasil")
+}
